3d0a14c49c527080ff89adbce672b3fa9daf73c3
Daniil <daniil.vodenejev@khk.ee>
2022-02-10 11:57:20 +0200

    "DELETE FROM" statement

--- db.js
+++ db.js
@@ -9,10 +9,10 @@ const con = mysql.createConnection( {
 
 con.connect(function (err){
     if(err) throw err;
-    console.log("Connected");
-    var sql = "CREATE TABLE customers (name VARCHAR (255), address VARCHAR (255))";
+
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
     con.query(sql, function (err, result){
         if(err) throw err;
-        console.log("database created");
+        console.log("Number of records deleted: " + result.affectedRows);
     });
 });
