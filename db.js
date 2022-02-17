1b5be760b7ecbbe358ed38f8bcea2746330585a1
Daniil <daniil.vodenejev@khk.ee>
2022-02-17 09:24:37 +0200

    select from table

--- db.js
+++ db.js
@@ -9,10 +9,8 @@ const con = mysql.createConnection( {
 
 con.connect(function (err){
     if(err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql, function (err, result) {
+    con.query("SELECT * FROM customers", function (err, result, fields) {
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log(result);
     });
 });
